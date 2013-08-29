88a0eb5275fc796d87866771f41520de4c3dbc6b
fresheneesz <bitetrudpublic@gmail.com>
2013-08-28 23:47:37 -0700

    adding files

---
+++ buildModules.js
@@ -0,0 +1,49 @@
+var fs = require('fs')
+var uglify = require('uglify-js')
+
+module.exports = buildOutput
+
+function minify(js, sourceMapName) {
+	return uglify.minify(js, {
+		fromString: true, 
+    	outSourceMap: sourceMapName+".map"
+	}).code
+}
+
+function globalify(commonJs) {
+	return '// requires: jquery\n'+
+	';(function(exports) {\n'+
+		commonJs+
+	'})(this)'
+}
+function amdify(commonJs) {
+	return "require(['jQuery'], function($) {\n"+
+		'var module.exports = {}, exports = module.exports\n'+
+		commonJs+
+		'return module.exports'
+	'})'
+}
+
+function buildOutput(buildDirectory, name, header, contents, filenames) {
+	if(!filenames) filenames = {}
+	if(!filenames.commonJs) filenames.commonJs = name+'.common.js'
+	var amdName = name+'.amd.js' 
+	if(!filenames.amd) filenames.amd = amdName 
+	if(!filenames.minAmd) filenames.minAmd = name+'.amd.min.js' 
+	var globalName = name+'.global.js'
+	if(!filenames.global) filenames.global = globalName
+	if(!filenames.minGlobal) filenames.minGlobal = name+'.global.min.js'
+	
+	var amd = amdify(contents)
+	var global = globalify(contents)
+	
+	write(filenames.commonJs, contents) 				// commonJs (raw)
+	write(filenames.amd, amd) 							// amd
+	write(filenames.minAmd, minify(amd, amdName)) 		// minified amd
+	write(filenames.global, global) 					// global
+	write(filenames.minGlobal, minify(global, globalName))	// minified global	
+	
+	function write(name, file) {
+		fs.writeFile(buildDirectory+name, header+'\n'+file)
+	}
+}
